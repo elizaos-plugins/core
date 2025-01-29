090126727676752f7d6af231e963b14c4ae49afe
Avaer Kazmer <ultimus@gmail.com>
2025-01-28 16:34:18 -0800

    Initial commit

---
+++ index.ts
@@ -0,0 +1,19 @@
+// this file is a stub
+// it should be marked as external and linked to the real implementation at runtime
+
+const anyFunction: (...args: any[]) => any = () => {};
+const anyClass: new (...args: any[]) => any = class {};
+
+export const generateText = anyFunction;
+export const composeContext = anyFunction;
+export const elizaLogger = anyFunction;
+export const splitChunks = anyFunction;
+export const trimTokens = anyFunction;
+
+export const ModelClass = anyClass;
+
+export type HandlerCallback = any;
+export type IAgentRuntime = any;
+export type Memory = any;
+export type State = any;
+export type Action = any;
